1ace5a1e10bd5213cc644138a32e524a3dcfe002
tomaspatane <tomas.patane@gmail.com>
2026-08-21 16:54:03 -0300

    Make StonedOwl v1.0 the root app safely

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,13 @@
-export { default } from '../worker-v10.js';
+import worker from '../worker-v10.js';
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    if (url.pathname === '/') {
+      url.pathname = '/v10.html';
+      return env.ASSETS.fetch(new Request(url.toString(), { headers: request.headers }));
+    }
+    return worker.fetch(request, env, ctx);
+  },
+  scheduled: worker.scheduled
+};
